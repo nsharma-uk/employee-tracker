c85cc348ad04de12d85b12cc6a8714fd2098335d
nsharma-uk <n.sharmauk711@gmail.com>
2022-06-26 20:41:52 +0100

    added employee table in schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -3,26 +3,28 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
-USE company_db;
-
-CREATE TABLE department (
-  id INTEGER NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INTEGER NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(50) NOT NULL,
   salary DECIMAL(15,2) NOT NULL,
-  department_id INTEGER,
+  department_id INT,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INTNOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
