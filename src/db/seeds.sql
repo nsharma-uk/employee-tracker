6fd6b92f5769de7e407b29b30bd1cea6d6bc8b02
nsharma-uk <n.sharmauk711@gmail.com>
2022-06-27 17:42:35 +0100

    seeded emp_roles

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -1,3 +1,7 @@
+
+
+
+
 INSERT INTO 
    department (dept_name)
 VALUES
@@ -5,6 +9,19 @@ VALUES
 ("Information Technology"),
 ("Human Resources"),
 ("Customer Services"),
-("Finance"),
-("Administration")
+("Finance")
+;
+
+INSERT INTO emp_role (title, salary, department_id)
+VALUES 
+("Marketing Manager", 60000.00, 1),
+("Marketing Assistant", 25000.00, 1),
+("Software Engineer", 60000.00, 2),
+("Junior Developer", 30000.00, 2),
+("HR Assistant", 25000.00, 3),
+("HR Manager", 25000.00, 3),
+("Customer Services Assistant", 25000.00, 4),
+("Customer Services Manager", 60000.00, 4),
+("Finance Assistant", 25000.00, 5),
+("Finance Manager", 60000.00, 5)
 ;
