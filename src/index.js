c14bb23a03b89cf6ccfe432b38a602d5d9ce5ec7
nsharma-uk <n.sharmauk711@gmail.com>
2022-06-27 22:39:54 +0100

    fixed file issues

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 require("dotenv").config();
+const mysql = require("mysql2");
 
-const inquirer = require("inquirer");
+//const inquirer = require("inquirer");
 
+//define DB config
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+//console.log(process.env);
+
+const db = mysql.createConnection(config);
+
+db.query("SELECT * FROM department", function (error, results) {
+  console.log(results); // results contains rows returned by server
+});
